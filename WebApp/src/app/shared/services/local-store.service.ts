12f3218b2cf29f1422ccac6cabcba2c544a3644e
Mitja <mh7860@student.uni-lj.si>
2020-03-31 21:01:39 +0200

    feat: prepare service for local storage

--- WebApp/src/app/shared/services/local-store.service.ts
+++ WebApp/src/app/shared/services/local-store.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LocalStoreService {
-  constructor() {}
+    public getItem(key: string): string {
+        return localStorage.getItem(key);
+    }
+
+    public saveItem(key: string, value: string): void {
+        localStorage.setItem(key, value);
+    }
+
+    public removeItem(key: string): void {
+        localStorage.removeItem(key);
+    }
 }
